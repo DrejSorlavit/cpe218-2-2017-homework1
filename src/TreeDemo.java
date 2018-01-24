d79e43702f023bed1ae9d977571d536331442169
J'erd <sorlavit.san@gmail.com>
2018-01-24 23:27:11 +0700

    GuiTreeDemo

---
+++ src/TreeDemo.java
@@ -0,0 +1,198 @@
+package homework1;
+
+import sun.reflect.generics.tree.Tree;
+
+import javax.swing.*;
+
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+import javax.swing.tree.TreeSelectionModel;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+
+
+import java.net.URL;
+import java.io.IOException;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+public class TreeDemo extends JPanel
+        implements TreeSelectionListener {
+    private JEditorPane htmlPane;
+    private JTree tree;
+    private URL helpURL;
+    private static boolean DEBUG = false;
+
+    private static boolean playWithLineStyle = false;
+    private static String lineStyle = "Horizontal";
+
+    private static boolean useSystemLookAndFeel = false;
+
+    public TreeDemo() {
+        super(new GridLayout(1,0));
+
+        //Create the nodes.
+        DefaultMutableTreeNode top =
+                new DefaultMutableTreeNode(Homework1.Tree);
+        createNodes(top,Homework1.Tree);
+
+        //Create a tree that allows one selection at a time.
+        tree = new JTree(top);
+        tree.getSelectionModel().setSelectionMode
+                (TreeSelectionModel.SINGLE_TREE_SELECTION);
+        //Listen for when the selection changes.
+        ImageIcon nodeTree = new ImageIcon(TreeDemo.class.getResource("middle.gif"));
+        DefaultTreeCellRenderer renderer = new DefaultTreeCellRenderer();
+        renderer.setClosedIcon(nodeTree);
+        renderer.setOpenIcon(nodeTree);
+        tree.setCellRenderer(renderer);
+        tree.addTreeSelectionListener(this);
+
+        if (playWithLineStyle) {
+            System.out.println("line style = " + lineStyle);
+            tree.putClientProperty("JTree.lineStyle", lineStyle);
+        }
+
+        //Create the scroll pane and add the tree to it.
+        JScrollPane treeView = new JScrollPane(tree);
+
+        //Create the HTML viewing pane.
+        htmlPane = new JEditorPane();
+        htmlPane.setEditable(false);
+        initHelp();
+        JScrollPane htmlView = new JScrollPane(htmlPane);
+
+        //Add the scroll panes to a split pane.
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+        splitPane.setTopComponent(treeView);
+        splitPane.setBottomComponent(htmlView);
+
+        Dimension minimumSize = new Dimension(100, 50);
+        htmlView.setMinimumSize(minimumSize);
+        treeView.setMinimumSize(minimumSize);
+        splitPane.setDividerLocation(100);
+        splitPane.setPreferredSize(new Dimension(500, 300));
+
+        //Add the split pane to this panel.
+        add(splitPane);
+    }
+
+    /** Required by TreeSelectionListener interface. */
+    public void valueChanged(TreeSelectionEvent e) {
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+                tree.getLastSelectedPathComponent();
+
+        if (node == null) return;
+
+        Object nodeInfo = node.getUserObject();
+        Homework1.Node nodeTree = (Homework1.Node)nodeInfo;
+        Homework1.inorder(nodeTree);
+        htmlPane.setText(nodeTree.key.toString());
+
+        if(nodeTree.left != null || nodeTree.right != null){
+            htmlPane.setText("" + Homework1.calculate(nodeTree));
+        }
+
+    }
+
+
+    private class BookInfo {
+        public String bookName;
+        public URL bookURL;
+
+        public BookInfo(String book, String filename) {
+            bookName = book;
+            bookURL = getClass().getResource(filename);
+            if (bookURL == null) {
+                System.err.println("Couldn't find file: "
+                        + filename);
+            }
+        }
+
+        public String toString() {
+            return bookName;
+        }
+    }
+
+    private void initHelp() {
+        String s = "TreeDemoHelp.html";
+        helpURL = getClass().getResource(s);
+        if (helpURL == null) {
+            System.err.println("Couldn't open help file: " + s);
+        } else if (DEBUG) {
+            System.out.println("Help URL is " + helpURL);
+        }
+
+        displayURL(helpURL);
+    }
+
+    private void displayURL(URL url) {
+        try {
+            if (url != null) {
+                htmlPane.setPage(url);
+            } else { //null url
+                htmlPane.setText("File Not Found");
+                if (DEBUG) {
+                    System.out.println("Attempted to display a null URL.");
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Attempted to read a bad URL: " + url);
+        }
+    }
+
+        public static boolean isOperator(String a){
+        switch(a){
+            case "+":return true;
+            case "-":return true;
+            case "*":return true;
+            case "/":return true;
+            default : return false;
+        }
+    } 
+    
+    private void createNodes(DefaultMutableTreeNode top, Homework1.Node n) {
+
+
+        if (isOperator(n.key)) {
+            DefaultMutableTreeNode l = new DefaultMutableTreeNode(n.left);
+            top.add(l);
+            createNodes(l, n.left);
+            DefaultMutableTreeNode r = new DefaultMutableTreeNode(n.right);
+            top.add(r);
+            createNodes(r, n.right);
+        }
+    }
+
+    
+    private static void createAndShowGUI() {
+        if (useSystemLookAndFeel) {
+            try {
+                UIManager.setLookAndFeel(
+                        UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                System.err.println("Couldn't use system look and feel.");
+            }
+        }
+
+        //Create and set up the window.
+        JFrame frame = new JFrame("TreeDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Add content to the window.
+        frame.add(new TreeDemo());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
